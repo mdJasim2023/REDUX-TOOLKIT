8d65b9a0b50f5ab5f20c68465d7c00fbab208ba1
mdJasim2023 <jasim127md@gmail.com>
2024-02-06 13:38:33 +0530

    bjkbg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Dream from './features/Dream-11/Dream-11';
 import P from './features/practice/p';
 
 function App() {
   return (
     <div className="App">
-      
+      <Dream></Dream>
       <P></P>
     </div>
   );
---
+++ src/features/Dream-11/Dream-11.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dream(){
+    return(
+        <div>
+            <h1>Dream-11</h1>
+        </div>
+    )
+}
+
+export default Dream
